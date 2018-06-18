97bb1f1902e12fd662185aab5c7d286fa3ac7a3b
stephan <stephansmit@hotmail.com>
2018-06-18 11:45:15 +0200

    added the generating of the figures to the runall script:

--- runall.sh
+++ runall.sh
@@ -2,5 +2,6 @@
 
 (cd PropertiesSU2 && make clean && make && ./createTables.out)
 (cd PropertiesCP && python createTable.py)
+(cd Figures && python createFigures.py)
 (cd Presentation && pdflatex presentation.tex)
 
