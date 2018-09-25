9cd60a3c6cdbb6bf11e8f7fab6893fc9cef1607c
stephan <stephansmit@hotmail.com>
2018-09-25 17:38:55 +0200

    latest

--- PropertiesCP_C++/main.cpp
+++ PropertiesCP_C++/main.cpp
@@ -1,156 +1,237 @@
-#include "CoolProp.h"
 #include <iostream>
 #include <cstdlib>
-struct FluidState {
-        double P;
-        double T;
-        double v;
-        double d;
-        double h;
-        double s;
-        double u;
-        double cv;
-        double cp;
-        double c;
-        double alpha;
-        double beta;
-        double zeta;
-        double eta;
-        double lambda;
-        int i;
-        int j;
-        int phase;
-        double dlam_dT;
-        double dlam_drho;
-        double deta_dT;
-        double deta_drho;
-        double dPdrho_e;
-        double dPde_rho;
-        double dTdrho_e;
-        double dTde_rho;
-        double dhdrho_P;
-        double dhdP_rho;
-        double dsdrho_P;
-        double dsdP_rho;
-};
-
-using namespace std;
-double calc_derivative(string property, string withrespect_to, string at_constant, string InputSpec, double input1, double input2, string fluid_name){
-     
-    double fph, fmh;
-    double eps=0.0001;
-    if (string(1,InputSpec[0])==withrespect_to) {
-    	fph = CoolProp::PropsSI(property, string(1,InputSpec[0]), input1+.5*eps, string(1,InputSpec[1]), input2, fluid_name);
-	fmh = CoolProp::PropsSI(property, string(1,InputSpec[0]), input1-.5*eps, string(1,InputSpec[1]), input2, fluid_name);
-    }
-    else if (string(1,InputSpec[1])==withrespect_to) {
-    	fph = CoolProp::PropsSI(property, string(1,InputSpec[0]), input1, string(1,InputSpec[1]), input2+.5*eps, fluid_name);
-	fmh = CoolProp::PropsSI(property, string(1,InputSpec[0]), input1, string(1,InputSpec[1]), input2-.5*eps, fluid_name);
-    }
-    return (fph-fmh)/eps;
-}
-void coolprop_allprops_su2(string InputSpec, string fluid_name, double input1, double input2, struct FluidState &state){
-    state.P=CoolProp::PropsSI("P", string(1,InputSpec[0]), input1, string(1,InputSpec[1]), input2, fluid_name);
-    state.T=CoolProp::PropsSI("T", string(1,InputSpec[0]), input1, string(1,InputSpec[1]), input2, fluid_name);
-    state.d=CoolProp::PropsSI("D", string(1,InputSpec[0]), input1, string(1,InputSpec[1]), input2, fluid_name);
-    state.v=1.0/state.d;
-    state.h=CoolProp::PropsSI("H", string(1,InputSpec[0]), input1, string(1,InputSpec[1]), input2, fluid_name);
-    state.s=CoolProp::PropsSI("S", string(1,InputSpec[0]), input1, string(1,InputSpec[1]), input2, fluid_name);
-    state.u=CoolProp::PropsSI("U", string(1,InputSpec[0]), input1, string(1,InputSpec[1]), input2, fluid_name);
-    state.cv=CoolProp::PropsSI("CVMASS", string(1,InputSpec[0]), input1, string(1,InputSpec[1]), input2, fluid_name);
-    state.cp=CoolProp::PropsSI("CPMASS", string(1,InputSpec[0]), input1, string(1,InputSpec[1]), input2, fluid_name);
-    state.c=CoolProp::PropsSI("A", string(1,InputSpec[0]), input1, string(1,InputSpec[1]), input2, fluid_name);
-    state.eta=CoolProp::PropsSI("V", string(1,InputSpec[0]), input1, string(1,InputSpec[1]), input2, fluid_name);
-    state.lambda=CoolProp::PropsSI("L", string(1,InputSpec[0]), input1, string(1,InputSpec[1]), input2, fluid_name);
-    state.phase=CoolProp::PropsSI("PHASE", string(1,InputSpec[0]), input1, string(1,InputSpec[1]), input2, fluid_name);
-    state.dlam_dT=calc_derivative("L", "T", "D", InputSpec, input1, input2, fluid_name);
-    state.dlam_drho=calc_derivative("L", "D", "T", InputSpec, input1, input2, fluid_name);
-    state.deta_dT=calc_derivative("V", "T", "D", InputSpec, input1, input2, fluid_name);
-    state.deta_drho=calc_derivative("V", "D", "T", InputSpec, input1, input2, fluid_name);
-    state.dPdrho_e=CoolProp::PropsSI("d(P)/d(D)|U", string(1,InputSpec[0]), input1, string(1,InputSpec[1]), input2, fluid_name);
-    state.dPde_rho=CoolProp::PropsSI("d(P)/d(U)|D", string(1,InputSpec[0]), input1, string(1,InputSpec[1]), input2, fluid_name);
-    state.dTdrho_e=CoolProp::PropsSI("d(T)/d(D)|U", string(1,InputSpec[0]), input1, string(1,InputSpec[1]), input2, fluid_name);
-    state.dTde_rho=CoolProp::PropsSI("d(T)/d(U)|D", string(1,InputSpec[0]), input1, string(1,InputSpec[1]), input2, fluid_name);
-    state.dhdrho_P=CoolProp::PropsSI("d(H)/d(D)|P", string(1,InputSpec[0]), input1, string(1,InputSpec[1]), input2, fluid_name);
-    state.dhdP_rho=CoolProp::PropsSI("d(H)/d(P)|D", string(1,InputSpec[0]), input1, string(1,InputSpec[1]), input2, fluid_name);
-    state.dsdrho_P=CoolProp::PropsSI("d(S)/d(D)|P", string(1,InputSpec[0]), input1, string(1,InputSpec[1]), input2, fluid_name);
-    state.dsdP_rho=CoolProp::PropsSI("d(S)/d(P)|D", string(1,InputSpec[0]), input1, string(1,InputSpec[1]), input2, fluid_name);
-}
+#include "../../SU2-fork/Common/include/mpi_structure.hpp"
+#include "../../SU2-fork/SU2_CFD/include/fluid_model.hpp"
+#include "../../SU2-fork/SU2_CFD/include/look_up_table.hpp"
+#include "../../SU2-fork/SU2_CFD/src/fluid_model_pig.cpp"
+#include "../../SU2-fork/SU2_CFD/src/look_up_table.cpp"
+#include "../../SU2-fork/SU2_CFD/src/fluid_model_lut.cpp"
+#include "../../SU2-fork/SU2_CFD/src/fluid_model_ppr.cpp"
+#include "../../SU2-fork/SU2_CFD/src/fluid_model_pvdw.cpp"
+#include "../../SU2-fork/SU2_CFD/src/fluid_model.cpp"
+#include "../../SU2-fork/SU2_CFD/src/fluid_model_cp.cpp"
+#include "../../SU2-fork/SU2_CFD/src/transport_model.cpp"
+#include "../../SU2-fork/SU2_CFD/include/transport_model.hpp"
+#include "../../SU2-fork/SU2_CFD/src/transport_model_toluene.cpp"
+#include "../../SU2-fork/SU2_CFD/src/transport_model_lut.cpp"
+#include "../../SU2-fork/SU2_CFD/src/transport_model_cp.cpp"
 
 
 
-int main()
+
+
+
+int main() 
 {
-    // First type (slowest, due to most string processing, exposed in DLL)
-    FluidState *state = new FluidState;
-    string InputSpec = "TD";
-    string fluid_name = "Toluene";
-    coolprop_allprops_su2( InputSpec, fluid_name, 500., .1,  *state);
-        cout << "P: " << state->P << endl;
-        cout << "T: " << state->T << endl;
-        cout << "v: " << state->v << endl;
-        cout << "d: " << state->d << endl;
-        cout << "h: " << state->h << endl;
-        cout << "s: " << state->s << endl;
-        cout << "u: " << state->u << endl;
-        cout << "cv: " << state->cv << endl;
-        cout << "cp: " << state->cp << endl;
-        cout << "c: " << state->c << endl;
-        cout << "alpha: " << state->alpha << endl;
-        cout << "beta: " << state->beta << endl;
-        cout << "zeta: " << state->zeta << endl;
-        cout << "eta: " << state->eta << endl;
-        cout << "lambda: " << state->lambda << endl;
-        cout << "i: " << state->i << endl;
-        cout << "j: " << state->j << endl;
-        cout << "phase: " << state->phase << endl;
-        cout << "dlam_dT: " << state->dlam_dT << endl;
-        cout << "dlam_drho: " << state->dlam_drho << endl;
-        cout << "deta_dT: " << state->deta_dT << endl;
-        cout << "deta_drho: " << state->deta_drho << endl;
-        cout << "dPdrho_e: " << state->dPdrho_e << endl;
-        cout << "dPde_rho: " << state->dPde_rho << endl;
-        cout << "dTdrho_e: " << state->dTdrho_e << endl;
-        cout << "dTde_rho: " << state->dTde_rho << endl;
-        cout << "dhdrho_P: " << state->dhdrho_P << endl;
-        cout << "dhdP_rho: " << state->dhdP_rho << endl;
-        cout << "dsdrho_P: " << state->dsdrho_P << endl;
-        cout << "dsdP_rho: " << state->dsdP_rho << endl;
-    return EXIT_SUCCESS;
+    su2double gamma = 1.06;			//
+    su2double R = 90.23;			//
+    su2double Pstar = 4126300.0; 	//critical pressure toluene
+    su2double Tstar = 591.75;		//critical temperature toluene
+    su2double w = 0.2657;
+    su2double kt_const = 0.08029;
+    su2double pr_const = 1;
+    su2double mu_ref = 1.716E-5;	//reference viscocity sutherland
+    su2double t_ref =273.15;		//reference temperature sutherland
+    su2double rho_min = 0.005;
+    su2double rho_max = 145.;
+    su2double T_min = 310.0;
+    su2double T_max = 690.0;  
+    bool createTable = 0;
+    ////transport propert
+    //CViscosityToluene *ViscocityModelToluene = new CViscosityToluene();
+    //CConductivityToluene *ConductivityModelToluene = new CConductivityToluene();
+    CLUTFluidModel *LookUpTable = new CLUTFluidModel("table","Toluene", "LOG", 400,400,rho_min, rho_max, T_min, T_max, "BILINEAR", createTable);
+    CLUTFluidModel *LookUpTable2 = new CLUTFluidModel("table","Toluene", "LOG", 400,400,rho_min, rho_max, T_min, T_max, "BILINEAR", createTable);
+    CLUTFluidModel *LookUpTable3 = new CLUTFluidModel("table","Toluene", "LOG", 400,400,rho_min, rho_max, T_min, T_max, "BILINEAR", createTable);
+    CLUTFluidModel *LookUpTable4 = new CLUTFluidModel("table","Toluene", "LOG", 400,400,rho_min, rho_max, T_min, T_max, "BILINEAR", createTable);
+    CLUTFluidModel *LookUpTable5 = new CLUTFluidModel("table","Toluene", "LOG", 400,400,rho_min, rho_max, T_min, T_max, "BILINEAR", createTable);
+    CLUTFluidModel *LookUpTable6 = new CLUTFluidModel("table","Toluene", "LOG", 400,400,rho_min, rho_max, T_min, T_max, "BILINEAR", createTable);
+    CCPFluidModel *CoolProp = new CCPFluidModel("Toluene");
+    CConductivityCP *conduct = new CConductivityCP("Toluene");
+    CViscosityCP *visc = new CViscosityCP("Toluene");
+    CoolProp->SetThermalConductivityModelDirect(conduct);
+    CoolProp->SetLaminarViscosityModelDirect(visc);
+
+    LookUpTable->SetTransportModelsLUTDirect();
+    LookUpTable2->SetTransportModelsLUTDirect();
+    LookUpTable3->SetTransportModelsLUTDirect();
+    LookUpTable4->SetTransportModelsLUTDirect();
+    LookUpTable5->SetTransportModelsLUTDirect();
+    LookUpTable6->SetTransportModelsLUTDirect();
+
+
+    
+    su2double h, s, P, T, rho, e, k;
+
+
+    LookUpTable->SetTDState_rhoT(145,690);
+    cout << LookUpTable->GetPressure() << '\n';
+    cout << LookUpTable->GetLaminarViscosity() << '\n';
+    
+    h =LookUpTable->GetStaticEnergy() +  LookUpTable->GetPressure()/LookUpTable->GetDensity(); 
+    s =LookUpTable->GetEntropy();
+    P =LookUpTable->GetPressure();
+    T =LookUpTable->GetTemperature();
+    rho =LookUpTable->GetDensity();
+    e =LookUpTable->GetStaticEnergy();
+    k =LookUpTable->GetThermalConductivity();
+    
+    
+    LookUpTable2->SetTDState_Prho(P,rho);
+    cout << LookUpTable2->GetPressure() << '\n';
+    cout << LookUpTable2->GetTemperature() << '\n';
+    cout << LookUpTable2->GetDensity() << '\n';
+    cout << LookUpTable2->GetLaminarViscosity() << '\n';
+    LookUpTable3->SetTDState_rhoe(rho,e);
+    cout << LookUpTable3->GetPressure() << '\n';
+    cout << LookUpTable3->GetTemperature() << '\n';
+    cout << LookUpTable3->GetDensity() << '\n';
+    cout << LookUpTable3->GetLaminarViscosity() << '\n';
+    LookUpTable4->SetTDState_PT(P,T);
+    cout << LookUpTable4->GetPressure() << '\n';
+    cout << LookUpTable4->GetTemperature() << '\n';
+    cout << LookUpTable4->GetDensity() << '\n';
+    cout << LookUpTable4->GetLaminarViscosity() << '\n';
+    LookUpTable5->SetTDState_Ps(P,s);
+    cout << LookUpTable5->GetPressure() << '\n';
+    cout << LookUpTable5->GetTemperature() << '\n';
+    cout << LookUpTable5->GetDensity() << '\n';
+    cout << LookUpTable5->GetLaminarViscosity() << '\n';
+    LookUpTable6->SetTDState_hs(h,s);
+    cout << LookUpTable6->GetPressure() << '\n';
+    cout << LookUpTable6->GetTemperature() << '\n';
+    cout << LookUpTable6->GetDensity() << '\n';
+    cout << LookUpTable6->GetLaminarViscosity() << '\n';
+
+
+    cout << T << '\t' << h << '\t' << s << '\t'<< P << '\t'<< e << '\n';
+    //CoolProp->SetTDState_rhoT(0.05,488);
+    //CoolProp->SetTDState_rhoT(rho_min,T_min);
+    //cout << CoolProp->GetEntropy() << '\n';
+    //cout << CoolProp->GetPressure() << '\n';
+    //CoolProp->SetTDState_rhoT(rho_max,T_min);
+    //cout << CoolProp->GetEntropy() << '\n';
+    //cout << CoolProp->GetPressure() << '\n';
+    //CoolProp->SetTDState_rhoT(rho_min,T_max);
+    //cout << CoolProp->GetEntropy() << '\n';
+    //cout << CoolProp->GetPressure() << '\n';
+    //CoolProp->SetTDState_rhoT(rho_max,T_max);
+    //cout << CoolProp->GetEntropy() << '\n';
+    //cout << CoolProp->GetPressure() << '\n';
+
+
+
+
+    //h =CoolProp->GetStaticEnergy() +  CoolProp->GetPressure()/CoolProp->GetDensity(); 
+    //cout << h << endl;
+    //Set rhoT
+    //CoolProp->SetTDState_rhoT(2,500);
+    //cout << CoolProp->GetLaminarViscosity() << '\n';
+    //P = CoolProp->GetPressure ();
+    //P = CoolProp->GetSoundSpeed ();
+    //P = CoolProp->GetSoundSpeed2 ();
+    //P = CoolProp->GetDensity ();
+    //P = CoolProp->GetEntropy ();
+    //P = CoolProp->GetStaticEnergy ();
+    //P = CoolProp->GetTemperature ();
+    //P = CoolProp->GetCp ();
+    //P = CoolProp->GetdPdrho_e ();
+    //P = CoolProp->GetdPde_rho ();
+    //P = CoolProp->GetdTdrho_e ();
+    //P = CoolProp->GetdTde_rho ();
+    //P = CoolProp->Getdhdrho_P ();
+    //P = CoolProp->GetdhdP_rho ();
+    //P = CoolProp->Getdsdrho_P ();
+    //P = CoolProp->GetdsdP_rho ();
+    //P = CoolProp->GetLaminarViscosity();
+    //P = CoolProp->Getdmudrho_T ();
+    //P = CoolProp->GetdmudT_rho ();
+    //P = CoolProp->GetThermalConductivity();
+    //P = CoolProp->Getdktdrho_T ();
+    //P = CoolProp->GetdktdT_rho ();
+
+    //h =CoolProp->GetStaticEnergy() +  LookUpTable->GetPressure()/LookUpTable->GetDensity(); 
+    //s =CoolProp->GetEntropy();
+    //P =CoolProp->GetPressure();
+    //T =CoolProp->GetTemperature();
+    //rho =CoolProp->GetDensity();
+    //e =CoolProp->GetStaticEnergy();
+    //k =CoolProp->GetThermalConductivity();
+
+    //CoolProp->SetTDState_rhoT(2,500);
+    //cout << CoolProp->GetLaminarViscosity() << '\n';
+    //
+    //
+    ////set hs
+    //CoolProp->SetTDState_hs(h,s);
+    //cout << CoolProp->GetLaminarViscosity() << '\n';
+    //CoolProp->SetTDState_rhoT(2,500);
+    //cout << CoolProp->GetLaminarViscosity() << '\n';
+   
+    ////set PT
+    //CoolProp->SetTDState_PT(P,T);
+    //cout << CoolProp->GetLaminarViscosity() << '\n';
+    //CoolProp->SetTDState_rhoT(2,500);
+    //cout << CoolProp->GetLaminarViscosity() << '\n';
+
+    ////Set rhoe
+    //CoolProp->SetTDState_rhoe(rho,e);
+    //cout << CoolProp->GetLaminarViscosity() << '\n';
+    //CoolProp->SetTDState_rhoT(2,500);
+    //cout << CoolProp->GetLaminarViscosity() << '\n';
+    //
+    ////Set Ps
+    //CoolProp->SetTDState_Ps(P,s);
+    //cout << CoolProp->GetLaminarViscosity() << '\n';
+    //CoolProp->SetTDState_rhoT(2,500);
+    //cout << CoolProp->GetLaminarViscosity() << '\n';
+
+    ////Set Prho
+    //CoolProp->SetEnergy_Prho(P,rho);
+    //cout << CoolProp->GetLaminarViscosity() << '\n';
+
+
+   ////fluid properties
+    //CIdealGas *FluidModelIG = new CIdealGas(gamma, R);
+    //CPengRobinson *FluidModelPR = new CPengRobinson(gamma, R, Pstar, Tstar, w);
+
+    ////tables
+    //CPropertiesTableSU2 *TablePR = new CPropertiesTableSU2(FluidModelPR, ConductivityModelToluene, ViscocityModelToluene);
+    //CPropertiesTableSU2 *TableIG = new CPropertiesTableSU2(FluidModelIG, ConductivityModelToluene, ViscocityModelToluene);
+
+    ////writing tables
+    //TablePR->GenerateTable_PT("../Tables/ToluenePR_PT.txt",18e5, 0.1e5, 180, 600, 480 , 340);
+    //TablePR->GenerateTable_rhoT("../Tables/ToluenePR_rhoT.txt",100, 0.1, 180, 600, 480 , 340);
+    //TableIG->GenerateTable_PT("../Tables/TolueneIG_PT.txt",18e5, 0.1e5, 180, 600, 480 , 340);
+    //TableIG->GenerateTable_rhoT("../Tables/TolueneIG_rhoT.txt",100, 0.1, 180, 600, 480 , 340);
+
+
+//    FluidModelPR->SetThermalConductivityModel2(ViscocityModelToluene);
+//    CVanDerWaalsGas *FluidModelVDW = new CVanDerWaalsGas(gamma, R, Pstar, Tstar);
+
+
+
+//    su2double T, rho;
+//    T=300.0;rho=0.0;
+//    ViscocityModelToluene->SetViscosity(T,rho);
+//    cout << ViscocityModelToluene->GetViscosity() << '\n';
+//    T=400.0;rho=0.0;
+//    ViscocityModelToluene->SetViscosity(T,rho);
+//    cout << ViscocityModelToluene->GetViscosity() << '\n';
+//    T=550.0;rho=0.0;
+//    ViscocityModelToluene->SetViscosity(T,rho);
+//    cout << ViscocityModelToluene->GetViscosity() << '\n';
+//    T=300.0;rho=865.0;
+//    ViscocityModelToluene->SetViscosity(T,rho);
+//    cout << ViscocityModelToluene->GetViscosity() << '\n';
+//    T=400.0;rho=770.0;
+//    ViscocityModelToluene->SetViscosity(T,rho);
+//    cout << ViscocityModelToluene->GetViscosity() << '\n';
+//    T=550.0;rho=550.0;
+//    ViscocityModelToluene->SetViscosity(T,rho);
+//    cout << ViscocityModelToluene->GetViscosity() << '\n';
+//
+
+    return 0;
 
 }
-
-
-//
-//void createTable()
-//{
-//    double Tmin = 300.;//DoubleParam("TABLE_TMIN");
-//    double Tmax = 400.;//oubleParam("TABLE_TMAX");
-//    double rhomin = 0.1;//getDoubleParam("TABLE_RHOMIN");
-//    double rhomax = 10.;//getDoubleParam("TABLE_RHOMAX");
-//
-//    imax = 100//getIntParam("TABLE_IMAX");
-//    jmax = 100//getIntParam("TABLE_JMAX");
-//
-//    // ============================== //
-//    //        Generate table(s)       //
-//    // ============================== //
-//    int method = 0;//getIntParam("INTERPOLATION_SCHEME", 0); // 0=bilinear, 1=polynomial
-//    tab_split = 0;//getBoolParam("TABLE_SPLIT", 0);
-//    tab_dist = "LOG";//getStringParam("TABLE_DISTR", "UNIFORM"); // UNIFORM, LOG
-//
-//
-//    if (method==0)        intScheme="BILINEAR";
-//
-//    // initialize the table
-//    state = new FluidState*[imax];
-//    for (int i=0; i<imax; i++)
-//      state[i] = new FluidState[jmax];
-//
-//
-//}
-//
-
-
-
-
---
+++ PropertiesCP_C++/main2.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+#include "../../SU2-fork/Common/include/mpi_structure.hpp"
+#include "../../SU2-fork/SU2_CFD/include/fluid_model.hpp"
+#include "../../SU2-fork/SU2_CFD/include/look_up_table.hpp"
+#include "../../SU2-fork/SU2_CFD/src/fluid_model_pig.cpp"
+#include "../../SU2-fork/SU2_CFD/src/look_up_table.cpp"
+#include "../../SU2-fork/SU2_CFD/src/fluid_model_lut.cpp"
+#include "../../SU2-fork/SU2_CFD/src/fluid_model_ppr.cpp"
+#include "../../SU2-fork/SU2_CFD/src/fluid_model_pvdw.cpp"
+#include "../../SU2-fork/SU2_CFD/src/fluid_model.cpp"
+#include "../../SU2-fork/SU2_CFD/src/transport_model.cpp"
+#include "../../SU2-fork/SU2_CFD/include/transport_model.hpp"
+#include "../../SU2-fork/SU2_CFD/src/transport_model_toluene.cpp"
+#include "../../SU2-fork/SU2_CFD/src/transport_model_lut.cpp"
+
+
+
+
+
+
+
+
+
+
+int main() 
+{
+    su2double gamma = 1.06;			//
+    su2double R = 90.23;			//
+    su2double Pstar = 4126300.0; 	//critical pressure toluene
+    su2double Tstar = 591.75;		//critical temperature toluene
+    su2double w = 0.2657;
+    su2double kt_const = 0.08029;
+    su2double pr_const = 1;
+    su2double mu_ref = 1.716E-5;	//reference viscocity sutherland
+    su2double s = 110.4;			//sutherland constant
+    su2double t_ref =273.15;		//reference temperature sutherland
+
+    ////transport propert
+    //CViscosityToluene *ViscocityModelToluene = new CViscosityToluene();
+    //CConductivityToluene *ConductivityModelToluene = new CConductivityToluene();
+    CLookUpTable *LookUpTable = new CLookUpTable("table.bin", "LOG", 400,400);
+    LookUpTable->SetTDState_rhoT(1,400);
+     
+    cout << LookUpTable->GetLaminarViscosity() << '\n';
+   ////fluid properties
+    //CIdealGas *FluidModelIG = new CIdealGas(gamma, R);
+    //CPengRobinson *FluidModelPR = new CPengRobinson(gamma, R, Pstar, Tstar, w);
+
+    ////tables
+    //CPropertiesTableSU2 *TablePR = new CPropertiesTableSU2(FluidModelPR, ConductivityModelToluene, ViscocityModelToluene);
+    //CPropertiesTableSU2 *TableIG = new CPropertiesTableSU2(FluidModelIG, ConductivityModelToluene, ViscocityModelToluene);
+
+    ////writing tables
+    //TablePR->GenerateTable_PT("../Tables/ToluenePR_PT.txt",18e5, 0.1e5, 180, 600, 480 , 340);
+    //TablePR->GenerateTable_rhoT("../Tables/ToluenePR_rhoT.txt",100, 0.1, 180, 600, 480 , 340);
+    //TableIG->GenerateTable_PT("../Tables/TolueneIG_PT.txt",18e5, 0.1e5, 180, 600, 480 , 340);
+    //TableIG->GenerateTable_rhoT("../Tables/TolueneIG_rhoT.txt",100, 0.1, 180, 600, 480 , 340);
+
+
+//    FluidModelPR->SetThermalConductivityModel2(ViscocityModelToluene);
+//    CVanDerWaalsGas *FluidModelVDW = new CVanDerWaalsGas(gamma, R, Pstar, Tstar);
+
+
+
+//    su2double T, rho;
+//    T=300.0;rho=0.0;
+//    ViscocityModelToluene->SetViscosity(T,rho);
+//    cout << ViscocityModelToluene->GetViscosity() << '\n';
+//    T=400.0;rho=0.0;
+//    ViscocityModelToluene->SetViscosity(T,rho);
+//    cout << ViscocityModelToluene->GetViscosity() << '\n';
+//    T=550.0;rho=0.0;
+//    ViscocityModelToluene->SetViscosity(T,rho);
+//    cout << ViscocityModelToluene->GetViscosity() << '\n';
+//    T=300.0;rho=865.0;
+//    ViscocityModelToluene->SetViscosity(T,rho);
+//    cout << ViscocityModelToluene->GetViscosity() << '\n';
+//    T=400.0;rho=770.0;
+//    ViscocityModelToluene->SetViscosity(T,rho);
+//    cout << ViscocityModelToluene->GetViscosity() << '\n';
+//    T=550.0;rho=550.0;
+//    ViscocityModelToluene->SetViscosity(T,rho);
+//    cout << ViscocityModelToluene->GetViscosity() << '\n';
+//
+
+    return 0;
+
+}
